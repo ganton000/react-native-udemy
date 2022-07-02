bae5b685b3426c51ca3993f9cca82754c3fc408d
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-01 20:09:00 -0400

    Added styling to StartGameScreen

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -1,16 +1,48 @@
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import React from "react";
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
-            <TextInput />
-			<PrimaryButton>Reset</PrimaryButton>
-			<PrimaryButton>Confirm</PrimaryButton>
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+				autoCorrect={false}
+            />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+        backgroundColor: "#72063c",
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
