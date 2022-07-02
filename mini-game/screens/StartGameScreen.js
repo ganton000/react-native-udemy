f9b2037ca12b60ce676d2e97f951416fb8db0f9c
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-02 19:55:38 -0400

    Fixed styling for buttons in StartGameScreen

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -11,10 +11,16 @@ const StartGameScreen = () => {
                 maxLength={2}
                 keyboardType="number-pad"
                 autoCapitalize="none"
-				autoCorrect={false}
+                autoCorrect={false}
             />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 };
@@ -23,6 +29,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
@@ -44,5 +52,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: "bold",
         textAlign: "center",
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+    },
+    buttonContainer: {
+        flex: 1,
     },
 });
