ef8a125cf5eaa1e030f0e7d2a4a09600580ba819
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-02 20:53:54 -0400

    Wrapped all screens in SafeAreaView to respect upper toolbar in mobile devices

--- mini-game/screens/GameScreen.js
+++ mini-game/screens/GameScreen.js
@@ -1,12 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const GameScreen = () => {
     return (
-        <View>
-            <Text>GameScreen</Text>
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
+            <View></View>
         </View>
     );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12,
+    },
+});
