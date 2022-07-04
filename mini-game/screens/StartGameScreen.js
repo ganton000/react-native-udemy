c7298559d198c83f938d4afe6b5c3aa59413677d
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-03 20:39:03 -0400

    Use KeyboardAvoidingView and ScrollView components to fix bug with user input area

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -4,6 +4,8 @@ import {
     StyleSheet,
     useWindowDimensions,
     Dimensions,
+    KeyboardAvoidingView,
+    ScrollView,
     Alert,
 } from "react-native";
 import React, { useState } from "react";
@@ -53,33 +55,42 @@ const StartGameScreen = ({ onPickedNumber }) => {
     const marginTopDistance = height < 380 ? 30 : 100;
 
     return (
-        <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
-            <Title>Guess My Number</Title>
-            <Card style={styles.inputContainer}>
-                <InstructionText>Enter a number</InstructionText>
-                <TextInput
-                    style={styles.numberInput}
-                    maxLength={2}
-                    keyboardType="number-pad"
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    onChangeText={numberInputHandler}
-                    value={enteredNumber}
-                />
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={resetInputHandler}>
-                            Reset
-                        </PrimaryButton>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton onPress={confirmInputHandler}>
-                            Confirm
-                        </PrimaryButton>
-                    </View>
+        <ScrollView style={styles.screen}>
+            <KeyboardAvoidingView style={styles.screen} behavior="position">
+                <View
+                    style={[
+                        styles.rootContainer,
+                        { marginTop: marginTopDistance },
+                    ]}
+                >
+                    <Title>Guess My Number</Title>
+                    <Card style={styles.inputContainer}>
+                        <InstructionText>Enter a number</InstructionText>
+                        <TextInput
+                            style={styles.numberInput}
+                            maxLength={2}
+                            keyboardType="number-pad"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            onChangeText={numberInputHandler}
+                            value={enteredNumber}
+                        />
+                        <View style={styles.buttonsContainer}>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={resetInputHandler}>
+                                    Reset
+                                </PrimaryButton>
+                            </View>
+                            <View style={styles.buttonContainer}>
+                                <PrimaryButton onPress={confirmInputHandler}>
+                                    Confirm
+                                </PrimaryButton>
+                            </View>
+                        </View>
+                    </Card>
                 </View>
-            </Card>
-        </View>
+            </KeyboardAvoidingView>
+        </ScrollView>
     );
 };
 
@@ -88,6 +99,9 @@ export default StartGameScreen;
 //const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+    },
     rootContainer: {
         flex: 1,
         //marginTop: deviceHeight < 380 ? 30 : 100,
