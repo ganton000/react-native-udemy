09afff2bc67bd5483e7a305f2946a4aca9efe93b
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-03 21:47:11 -0400

    Used Platform API to update border width on Title

--- mini-game/components/ui/Title.js
+++ mini-game/components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 import React from "react";
 
 const Title = ({ children }) => {
@@ -13,7 +13,8 @@ const styles = StyleSheet.create({
         fontSize: 24,
         color: "white",
         textAlign: "center",
-        borderWidth: 2,
+        //borderWidth: Platform.OS === "android" ? 2 : 0,
+        borderWidth: Platform.select({ ios: 0, android: 2 }),
         borderColor: "white",
         padding: 12,
         maxWidth: "80%",
